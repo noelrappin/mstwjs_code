a7deaba53f8b00b268b3afb1d0dbd2f9405d7130
Noel Rappin <noel@groupon.com>
2012-05-11 12:42:03 -0500

    testing the toggler

---
+++ spec/javascripts/jasmine_examples/toggler_spec.rb
@@ -0,0 +1,9 @@
+describe("General Trip Index Page", function() {
+  
+  describe("clicking a show description link", function() {
+    beforeEach(function() {
+      loadFixtures("one_index_trip.html");
+      $("#trip_1 .detail_toggle").click(); // <label id="code.click_one" />
+    });
+  });
+};
