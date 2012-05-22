ff37d5e826828780caa52818a2b76756c0eb0f19
Noel Rappin <noel@groupon.com>
2012-05-21 23:55:23 -0500

    now with extra default preventions

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,12 +1,12 @@
 function init() {
   var $toggle_links = $(".detail_toggle");
-  $toggle_links.click(function() {
+  $toggle_links.click(function(event) {
     $detail = $(this).siblings(".detail");
-    $detail.toggleClass("hidden");    
+    $detail.toggleClass("hidden");   
+    event.preventDefault();
   });
 }
 
 $(function() {    
-  init()
+  init();
 })
-
