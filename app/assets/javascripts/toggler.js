6cf587300d3530156fba0a45dd277b720f5deccc
Noel Rappin <noel@groupon.com>
2012-05-23 00:05:51 -0500

    dumb test fix

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -2,11 +2,12 @@ function init() {
   var $toggle_links = $(".detail_toggle");
   $toggle_links.click(function() {
     $detail = $(this).siblings(".detail");
-    $detail.toggleClass("hidden");    
+    $detail.toggleClass("hidden"); 
+    $(this).text("Hide Details")   
   });
 }
 
 $(function() {    
-  init()
+  init();
 })
 
