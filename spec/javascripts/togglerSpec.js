50f2dea0c817f56758e80f393f52bc3704654a4c
Noel Rappin <noel@groupon.com>
2012-05-26 09:27:29 -0500

    added two more specs

--- spec/javascripts/togglerSpec.js
+++ spec/javascripts/togglerSpec.js
@@ -12,4 +12,18 @@ describe("clicking a show description link", function() {
   it('changes the link action to "Hide"',function() {
     expect($('.detail_toggle')).toHaveText("Hide Details"); 
   });
+
+  describe('clicking the link again', function() {
+    beforeEach(function(){
+      $(".detail_toggle").click(); 
+    });
+
+    it('hides the description', function() {   
+      expect($('.detail')).toHaveClass("hidden");
+    });
+
+    it('Changes the link action back to "Show"',function(){
+      expect($('.detail_toggle')).toHaveText("Show Details");
+    });
+  });
 });
