d7140553328bfd7484d82b2c99372eeedc535165
Noel Rappin <noel@groupon.com>
2012-06-13 07:36:19 -0500

    first autocomplete spec

---
+++ app/assets/javascripts/autocompleteSelector.js
@@ -0,0 +1,9 @@
+var initializeAutocompleteSelectors = function(options) {
+  var $parent = $(options.parentSelector);
+  var field = options.field;
+  var id = field.replace("][", "_").replace("[", "").replace("]", "");
+  var input = $("<input type='hidden'/>)")
+      .attr({"id": id, "name": field})
+      .val(options.initialValue);
+  $parent.append(input);
+};
---
+++ spec/javascripts/autocompleteSpec.js
@@ -0,0 +1,31 @@
+describe("Autocomplete widget", function() {
+  beforeEach(function() {
+    affix("form div#autodiv");
+    autocompleteData = {1: "Alpha", 2: "Beta", 3: "Gamma", 4: "Delta"};
+    initializeAutocompleteSelectors({
+        parentSelector: "#autodiv",
+        field: "[user][activity_ids]", 
+        initialValue: "1,3",
+        dataUniverse: autocompleteData});
+  });
+
+  //##marker
+  describe("sets up expected hidden element", function() {
+    beforeEach(function() {    
+      hiddenField = $("#autodiv #user_activity_ids");
+    });
+
+    it("gives the hidden field the correct type", function() {
+      expect(hiddenField).toHaveAttr("type", "hidden");
+    });
+    
+    it("gives the hidden field the correct name", function() {
+      expect(hiddenField).toHaveAttr("name", "[user][activity_ids]");
+    });
+    
+    it("gives the hidden field the correct value", function() {
+      expect(hiddenField).toHaveAttr("value", "1,3");
+    });
+  });
+  //##marker
+});
