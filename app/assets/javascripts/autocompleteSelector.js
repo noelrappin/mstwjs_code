1c1db69ca8351e1824a6c2392e0522277a489297
Noel Rappin <noel@groupon.com>
2012-06-14 22:30:36 -0500

    now as a widget

--- app/assets/javascripts/autocompleteSelector.js
+++ app/assets/javascripts/autocompleteSelector.js
@@ -1,31 +1,53 @@
-var initializeHiddenField = function(options) {
-  var $parent = $(options.parentSelector);
-  var field = options.field;
-  var id = field.replace("][", "_").replace("[", "").replace("]", "");
-  var input = $("<input type='hidden'/>)")
-      .attr({"id": id, "name": field})
-      .val(options.initialValue);
-  $parent.append(input);
-};
+var AutocompleteSelector = function() {
+  
+  var Constructor = function(options) { 
+    this.domParent = options.parentSelector;
+    this.options = options;
+    this.field = options.field;
+    this.universe = options.dataUniverse;
+    $(this.domParent).append(this.hiddenField())
+        .append(this.textInput())
+        .append(this.addButton());
+  }
+  
+  Constructor.prototype = {
+    determineId: function(suffix) {
+      var id = this.field.replace("][", "_") .replace("[", "") .replace("]", "");
+      if(suffix) {
+        id = id + "_" + suffix;
+      }
+      return id
+    },
+    
+    initialValue: function() {
+      return this.options.initialValue;
+    },
+    
+    hiddenField: function() {
+      return $("<input type='hidden'/>") 
+          .attr("id", this.determineId())
+          .attr("name", this.field)
+          .val(this.initialValue());
+    },
+    
+    textInput: function() {
+      return $("<input type='text'/>") 
+          .attr("id", this.determineId("autocomplete"))
+          .attr("name", this.field + "[autocomplete]");
+    },
+    
+    addButton: function() {
+      return $("<a href='#'>")
+          .attr("id", this.determineId("add_button"))
+          .html("Add")
+          .addClass('selector_add_button');
+    }
+  } 
+  
+  return Constructor;
+}();
 
-//##marker
-var initializeTextInput = function(options) {
-  var $parent = $(options.parentSelector);
-  var field = options.field;
-  var id = field.replace("][", "_") .replace("[", "") .replace("]", "");
-  var input = $("<input type='text'/>") 
-      .attr({"id": id + "_autocomplete", 
-          "name": field + "[autocomplete]"});
-  $parent.append(input);
-  var add_button = $("<a href='#'>")
-      .attr("id", id + "_add_button")
-      .html("Add")
-      .addClass('selector_add_button');
-  $parent.append(add_button);
-}
 
 var initializeAutocompleteSelector = function(options) {
-  initializeHiddenField(options);
-  initializeTextInput(options);
+  new AutocompleteSelector(options);
 }
-//##marker
