6f002b3c869c6ca62197ed4d71d2ae56bc50e3cf
Noel Rappin <noel@groupon.com>
2012-08-15 08:19:29 -0500

    naming convention fix

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,34 +1,34 @@
 var toggler = {
-  linkSelector: ".detail_toggle", 
+  linkSelector: ".detail_toggle",
   detailSelector: ".detail",
   hiddenClass: "hidden",
   hideText: "Hide Details",
-  showText: "Show Details", 
+  showText: "Show Details",
 
-  init: function() { 
-    var toggler = this; 
-    $(this.linkSelector).on("click", function(event) { 
-      toggler.toggleOnClick(event) 
-    });  
+  init: function() {
+    var toggler = this;
+    $(this.linkSelector).on("click", function(event) {
+      toggler.toggleOnClick(event)
+    });
   },
 
   toggleOnClick: function(event) {
-    this.link = $(event.target); 
-    this.link.text(this.isDetailHidden() ? this.hideText : this.showText);
+    this.$link = $(event.target);
+    this.$link.text(this.isDetailHidden() ? this.hideText : this.showText);
     this.detailElement().toggleClass(toggler.hiddenClass);
     event.preventDefault();
   },
 
-  detailElement: function() { 
+  detailElement: function() {
     return this.link.parent().find(this.detailSelector)
   },
 
-  isDetailHidden: function() { 
+  isDetailHidden: function() {
     return this.detailElement().hasClass(this.hiddenClass);
   },
 };
 
-$(function() {  
-  toggler.init(); 
+$(function() {
+  toggler.init();
 });
 
