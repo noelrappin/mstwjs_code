1523454c57e08baa0861796a1d90f7d361008803
Noel Rappin <noel@groupon.com>
2012-09-01 14:37:59 -0500

    manifest fix

--- app/assets/javascripts/backbone/time_travel.js
+++ app/assets/javascripts/backbone/time_travel.js
@@ -1,8 +1,8 @@
-#= require_self
-#= require_tree ./templates
-#= require_tree ./models
-#= require_tree ./views
-#= require_tree ./routers
+//= require_self
+//= require_tree ./templates
+//= require_tree ./models
+//= require_tree ./views
+//= require_tree ./routers
 
 var TimeTravel = {
   Models: {},
