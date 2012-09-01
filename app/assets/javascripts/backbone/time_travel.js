e4be95fed9726541d4f0655c6d5a53a3c6d6617d
Noel Rappin <noel@groupon.com>
2012-09-01 14:10:06 -0500

    fix to time_travel file

---
+++ app/assets/javascripts/backbone/time_travel.js
@@ -0,0 +1,16 @@
+#= require_self
+#= require_tree ./templates
+#= require_tree ./models
+#= require_tree ./views
+#= require_tree ./routers
+
+var TimeTravel = {
+  Models: {},
+  Collections: {},
+  Routers: {},
+  Views: {},
+
+  init: function(tripData) {
+    Backbone.history.start({pushState: true});
+  }
+}
