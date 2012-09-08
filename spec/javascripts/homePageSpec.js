ac70a85486e16b1c616c01d55c7ecfdf351dc817
Noel Rappin <noel@groupon.com>
2012-09-08 14:54:34 -0500

    fix for weird layout issue

--- spec/javascripts/homePageSpec.js
+++ spec/javascripts/homePageSpec.js
@@ -1,6 +1,13 @@
 describe("rendering the home page with Backbone", function() {
 
-  var tripData = '[{"description":"A cruise","end_date":"1620-11-21", "id":13, "image_name":"mayflower.jpg", "name":"Mayflower Luxury Cruise", "price":1204.0, "start_date":"1620-05-17", "tag_line":"Enjoy The Cruise That Started It All"}, {"description":"See plays", "end_date":"1605-10-31", "id":14, "image_name":"shakespeare.jpg", "name":"See Shakespeare\'s Plays", "price":1313.0", start_date":"1604-11-01", "tag_line":"See The Master As Intended"}]';
+  var tripData = '[{"description":"A cruise","end_date":"1620-11-21", ' +
+    '"id":13, "image_name":"mayflower.jpg", "name":"Mayflower Luxury Cruise", ' +
+    ' "price":1204.0, "start_date":"1620-05-17", ' +
+    '"tag_line":"Enjoy The Cruise That Started It All"}, ' +
+    '{"description":"See plays", "end_date":"1605-10-31", "id":14,' +
+    '"image_name":"shakespeare.jpg", "name":"See Shakespeare\'s Plays", ' +
+    '"price":1313.0", start_date":"1604-11-01", ' +
+    '"tag_line":"See The Master As Intended"}]';
 
   describe("end to end", function() {
 
