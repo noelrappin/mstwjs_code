95efa92861946fb153df76146883b34497a3d05d
Noel Rappin <noel@groupon.com>
2012-09-29 22:00:53 -0500

    model changes

--- app/assets/javascripts/backbone/models/trip.js
+++ app/assets/javascripts/backbone/models/trip.js
@@ -1,10 +1,18 @@
 //##trip
 TimeTravel.Models.Trip = Backbone.Model.extend({
 
+  defaults: {
+    'detailsDisplayed': false
+  },
+
   initialize: function(attributes) {
     this.set({startMoment: moment(attributes.start_date),
         endMoment: moment(attributes.end_date)});
-  }
+  },
+
+  toggleDetails: function() {
+    this.set({'detailsDisplayed': !this.get('detailsDisplayed')})
+  },
 
 });
 //##trip
