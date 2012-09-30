18ff20a6534fdd6467a8d5628ba92bdabc39d9fd
Noel Rappin <noel@groupon.com>
2012-09-29 22:47:19 -0500

    acceptance test working

--- app/assets/javascripts/backbone/views/tripView.js
+++ app/assets/javascripts/backbone/views/tripView.js
@@ -2,8 +2,17 @@ TimeTravel.Views.TripView = Backbone.View.extend({
   className: 'trip trip_entry span-6',
 
   //##init
+  events: {
+    'click .trip_detail_link': 'toggleDetails'
+  },
+
   initialize: function () {
     _.bindAll(this, 'render', 'formatDate', 'presentTrip', 'formatPrice');
+    this.model.bind('change:detailsDisplayed', this.render, this);
+  },
+
+  toggleDetails: function() {
+    this.model.toggleDetails();
   },
   //##init
 
@@ -11,6 +20,7 @@ TimeTravel.Views.TripView = Backbone.View.extend({
   render: function() {
     this.$el.html(TimeTravel.template(
         'tripViewTemplate').render(this.presentTrip()));
+    this.displayDetails();
     return this;
   },
   //##render
@@ -25,6 +35,18 @@ TimeTravel.Views.TripView = Backbone.View.extend({
   formatPrice: function(aFloat) {
     return "$" + aFloat.toFixed(2).toLocaleString();
   },
+
+  //##displayDetails
+  displayDetails: function() {
+    if(this.model.get("detailsDisplayed")) {
+      this.$el.find(".trip_detail_link").html("Hide Details");
+      this.$el.find(".trip_details").removeClass("hidden");
+    } else {
+      this.$el.find(".trip_detail_link").html("Show Details");
+      this.$el.find(".trip_details").addClass("hidden");
+    }
+  },
+  //##displayDetails
 
   presentTrip: function() {
     var result = this.model.toJSON();
--- spec/javascripts/tripViewSpec.js
+++ spec/javascripts/tripViewSpec.js
@@ -50,11 +50,33 @@ describe("with a trip view", function() {
 
     it("shows the details on a click", function() {
       this.tripView.render();
-      $el = this.tripView.$el;
+      var $el = this.tripView.$el;
       $el.find(".trip_detail_link").click();
       expect($el.find(".trip_details")).not.toHaveClass("hidden");
     });
   });
   //##toggleAccept
+
+  //##toggleUnit
+  describe("uses the display details behavior", function() {
+    it("hides details given state", function() {
+      this.tripView.model.set({'detailsDisplayed': false});
+      this.tripView.render();
+      var $el = this.tripView.$el;
+      expect($el.find(".trip_details")).toHaveClass("hidden");
+      expect($el.find(".trip_detail_link")).toHaveText("Show Details");
+    });
+
+    it("shows details given state", function() {
+      this.tripView.model.set({'detailsDisplayed': true});
+      this.tripView.render();
+      var $el = this.tripView.$el;
+      expect($el.find(".trip_details")).not.toHaveClass("hidden");
+      expect($el.find(".trip_detail_link")).toHaveText("Hide Details");
+    });
+  })
+  //##toggleUnit
+
+
 
 });
