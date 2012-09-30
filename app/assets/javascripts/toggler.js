77b8bbf5b119d2af99e75ebc2313df59ac947dbe
Noel Rappin <noel@groupon.com>
2012-09-30 10:35:50 -0500

    prevent devault is back

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -2,16 +2,17 @@ function init() {
   var $toggle_links = $(".detail_toggle");
   $toggle_links.click(function() {
     $detail = $(this).siblings(".detail");
-    if($detail.hasClass("hidden")) {         
-      $(this).text("Hide Details");      
+    if($detail.hasClass("hidden")) {
+      $(this).text("Hide Details");
     } else {
       $(this).text("Show Details");
     }
-    $detail.toggleClass("hidden"); 
+    $detail.toggleClass("hidden");
+    event.preventDefault();
   });
 }
 
-$(function() {    
+$(function() {
   init();
 })
 
