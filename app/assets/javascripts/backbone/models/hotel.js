b30b58cc75064162149d2e96384ab43310400277
Noel Rappin <noel@groupon.com>
2012-11-08 14:02:33 -0600

    testing URL logic

--- app/assets/javascripts/backbone/models/hotel.js
+++ app/assets/javascripts/backbone/models/hotel.js
@@ -4,6 +4,15 @@ TimeTravel.Models.Hotel = Backbone.Model.extend({
 
 TimeTravel.Collections.Hotels = Backbone.Collection.extend({
   model: TimeTravel.Models.Hotel,
-  url: "/hotels"
+
+  initialize: function(models, options) {
+    if (options && options.trip) {
+      this.trip = options.trip
+    }
+  },
+
+  url: function() {
+    return "/trips?trip_id=" + this.trip.get("id");
+  },
 });
 
