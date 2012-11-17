78595e258939ab80a3be2640970c55a4990358d9
Noel Rappin <noel@groupon.com>
2012-11-17 11:01:27 -0600

    first acceptance test

---
+++ spec/javascripts/tripDetailViewSpec.js
@@ -0,0 +1,22 @@
+describe("with a trip detail view", function() {
+  beforeEach(function() {
+    this.trip = new TimeTravel.Models.Trip({"description":"A cruise",
+      "end_date":"1620-11-21", "id":13, "image_name":"mayflower.jpg",
+      "name":"Mayflower Luxury Cruise", "price": 1204.0,
+      "start_date":"1620-05-17",
+      "tag_line":"Enjoy The Cruise That Started It All"});
+    this.tripDetailView = new TimeTravel.Views.TripDetailView(
+        {model: this.trip});
+  });
+
+  describe("acceptance tests", function() {
+    it("displays hotels", function() {
+      this.trip.hotels = [
+        new TimeTravel.Models.Hotel({"name": "Lux"}),
+        new TimeTravel.Models.Hotel({"name": "Third class"})
+      ];
+      this.tripDetailView.render();
+      expect(this.tripDetailView.$el.find(".hotel").length).toEqual(2);
+    });
+  });
+});
