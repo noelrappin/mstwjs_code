482b86f818194293a997d46c4eb9a9f933f6b1fe
Ricardo Mendes <rokusu@gmail.com>
2013-03-20 09:52:42 +0000

    typo, missing some ls

--- spec/javascripts/specs/models/extra_spec.js
+++ spec/javascripts/specs/models/extra_spec.js
@@ -4,8 +4,8 @@ describe("Extras", function() {
     Ember.testing = true;
   });
 
-  describe("price calcuation", function() {
-    it("calcuates the total revenue", function() {
+  describe("price calculation", function() {
+    it("calculates the total revenue", function() {
       Ember.run(function() {
         extra = TimeTravel.Extra.createRecord({price: "100", orders: "3"});
         expect(extra.get("revenue")).toEqual(300);
