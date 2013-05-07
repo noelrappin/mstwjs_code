df49af7764addf71f70638feb22e119a6505e0a3
Noel Rappin <noel@groupon.com>
2013-05-07 00:15:27 -0500

    code marker

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -2,7 +2,9 @@ TimeTravel.Router.reopen({
   location: "history"
 })
 
+//##jasmine
 TimeTravel.Router.map(function(match) {
   this.route("index", {path: "/"});
   this.route("jasmine", {path: "/jasmine"});
 });
+//##jasmine
