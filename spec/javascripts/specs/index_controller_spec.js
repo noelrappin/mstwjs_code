e92e780c4cd9eab674bed311b87b5f59bf0e5031
Noel Rappin <noel@groupon.com>
2013-05-08 23:48:49 -0500

    test refactoring

--- spec/javascripts/specs/index_controller_spec.js
+++ spec/javascripts/specs/index_controller_spec.js
@@ -1,37 +1,44 @@
+//##setup
+function setUpController(controller_class, content, templateName) {
+  var controller = controller_class.create({
+    container: TimeTravel.__container__,
+    content: content
+  });
+  var view = Ember.View.create({
+    controller: controller,
+    context: controller,
+    templateName: templateName
+  });
+  view.append();
+  return {controller: controller, view: view}
+}
+//##setup
+
+//##before
 describe("with an index controller", function() {
 
   beforeEach(function() {
     Ember.testing = true;
     TimeTravel.reset();
     Ember.run(function() {
-      this.trip = TimeTravel.Trip.createRecord({
-        name: "Mayflower",
-        startDate: "1620-09-06",
-        endDate: "1620-11-21"
-      });
-      controller = TimeTravel.IndexController.create({
-        container: TimeTravel.__container__,
-        content: [this.trip]
-      });
-      window.view = Ember.View.create({
-        controller: controller,
-        context: controller,
-        templateName: 'index'
-      });
-      this.view.append();
+      trip = TimeTravel.Trip.createRecord(
+          {name: "Mayflower", startDate: "1620-09-06", endDate: "1620-11-21"});
+      testData = setUpController(TimeTravel.IndexController, [trip], 'index');
     });
   });
 
   afterEach(function() {
-    Ember.run(function() { window.view.remove() });
+    Ember.run(function() { testData.view.remove() });
     Ember.testing = false;
   });
+//##before
 
+//##test
   describe("basic stuff", function() {
     it("displays trips", function() {
       Ember.run(function() {
-        expect($(".trip").length).toEqual(1)
-        expect($(".trip .dates").text()).toEqual("Sep 6, 1620 - Nov 21, 1620")
+        expect($(".trip").length).toEqual(1);
+        expect($(".trip .dates").text()).toEqual("Sep 6, 1620 - Nov 21, 1620");
       });
     });
 
@@ -45,3 +52,4 @@ describe("with an index controller", function() {
   });
 
 });
+//##test
