8218c0597f016044c9f015dea3aed7e1bf839c8d
Noel Rappin <noel@groupon.com>
2013-05-21 21:12:46 -0500

    typo fix

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,6 +1,6 @@
 function init() {
   var $toggle_links = $(".detail_toggle");
-  $toggle_links.click(function() {
+  $toggle_links.click(function(event) {
     $detail = $(this).siblings(".detail");
     $detail.toggleClass("hidden");
     $(this).text("Hide Details");
