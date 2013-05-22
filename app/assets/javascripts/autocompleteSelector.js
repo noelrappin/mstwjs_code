18524df3a865aff7416db02781b757d29038565c
Noel Rappin <noel@groupon.com>
2013-05-21 21:20:09 -0500

    typo fix

--- app/assets/javascripts/autocompleteSelector.js
+++ app/assets/javascripts/autocompleteSelector.js
@@ -2,7 +2,7 @@ var initializeAutocompleteSelector = function(options) {
   var $parent = $(options.parentSelector);
   var field = options.field;
   var id = field.replace("][", "_").replace("[", "").replace("]", "");
-  var input = $("<input type='hidden'/>)")
+  var input = $("<input type='hidden'/>")
       .attr({"id": id, "name": field})
       .val(options.initialValue);
   $parent.append(input);
