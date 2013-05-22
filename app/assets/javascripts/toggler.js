84d617734149c068859c48074a435ffa8db218b1
Noel Rappin <noel@groupon.com>
2013-05-21 21:52:32 -0500

    oops

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,7 +1,7 @@
 function init() {
   var $toggle_links = $(".detail_toggle");
   $toggle_links.click(function(event) {
-    $detail = $(this).siblings(".detail");
+    var $detail = $(this).siblings(".detail");
     $detail.toggleClass("hidden");
     $(this).text("Hide Details");
     event.preventDefault();
