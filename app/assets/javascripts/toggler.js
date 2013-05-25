3ec9e593de1ea5ba63a320f1ac057740f1e2b2c4
Noel Rappin <noel@groupon.com>
2013-05-25 10:01:31 -0500

    missing var

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,12 +1,12 @@
 function init() {
   var $toggle_links = $(".detail_toggle");
   $toggle_links.click(function(event) {
-    $detail = $(this).siblings(".detail");
-    $detail.toggleClass("hidden");   
+    var $detail = $(this).siblings(".detail");
+    $detail.toggleClass("hidden");
     event.preventDefault();
   });
 }
 
-$(function() {    
+$(function() {
   init();
 })
