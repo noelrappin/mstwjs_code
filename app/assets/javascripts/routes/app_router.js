974c6d774b0adb3cb8efe435be0ae8ed16edf65d
Noel Rappin <noel@groupon.com>
2013-06-12 23:37:51 -0500

    new routes

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -4,7 +4,14 @@ TimeTravel.Router.reopen({
 
 TimeTravel.Router.map(function() {
   this.resource("trips", {path: "/trips"}, function() {
-    this.resource("trip", {path: ":trip_id"});
+    this.resource("trip", {path: ":trip_id"}, function() {
+      this.resource("hotels", {path: "/hotels"}, function() {
+        this.resource("hotel", {path: ":hotel_id"})
+      });
+      this.resource("extras", {path: "/extras"}, function() {
+        this.resource("extra", {path: ":extra_id"})
+      });
+    });
   });
   this.route("jasmine", {path: "/jasmine"});
 });
