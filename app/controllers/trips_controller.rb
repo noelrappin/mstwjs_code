91551e84c1c6b372209775ccdfd0d2e5b2b59280
Noel Rappin <noel@groupon.com>
2013-06-16 12:33:39 -0500

    still fixing

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,5 @@
 class TripsController < ApplicationController
-  # GET /trips
-  # GET /trips.json
-  ##index
+
   def index
     @trips = Trip.all
     respond_to do |format|
@@ -11,10 +9,6 @@ class TripsController < ApplicationController
 
   end
 
-  ##index
-
-  # GET /trips/1
-  # GET /trips/1.json
   def show
     @trip = Trip.find(params[:id])
 
@@ -24,81 +18,4 @@ class TripsController < ApplicationController
     end
   end
 
-  VALUES = [0, 3, 4, 2, 5, 6]
-
-  def rating
-    @trip = Trip.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json:
-          {values: VALUES, id: @trip.id} }
-    end
-  end
-
-  def update_rating
-    @trip = Trip.find(params[:id])
-    VALUES[params[:rating].to_i] += 1
-    response = {values: VALUES, id: @trip.id}
-    render json: response
-  end
-
-  # GET /trips/new
-  # GET /trips/new.json
-  def new
-    @trip = Trip.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @trip }
-    end
-  end
-
-  # GET /trips/1/edit
-  def edit
-    @trip = Trip.find(params[:id])
-  end
-
-  # POST /trips
-  # POST /trips.json
-  def create
-    @trip = Trip.new(params[:trip])
-
-    respond_to do |format|
-      if @trip.save
-        format.html { redirect_to @trip, notice: 'Trip was successfully created.' }
-        format.json { render json: @trip, status: :created, location: @trip }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /trips/1
-  # PUT /trips/1.json
-  def update
-    @trip = Trip.find(params[:id])
-
-    respond_to do |format|
-      if @trip.update_attributes(params[:trip])
-        format.html { redirect_to @trip, notice: 'Trip was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /trips/1
-  # DELETE /trips/1.json
-  def destroy
-    @trip = Trip.find(params[:id])
-    @trip.destroy
-
-    respond_to do |format|
-      format.html { redirect_to trips_url }
-      format.json { head :ok }
-    end
-  end
 end
