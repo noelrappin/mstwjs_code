a6c2af5adf7e0fa1582dce722376043966e4b419
Noel Rappin <noel@groupon.com>
2013-07-11 09:18:50 -0500

    serializer fix

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,6 +1,6 @@
 class TripSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :name, :description, :start_date, :end_date,
+  attributes :name, :description, :start_date, :end_date, :id,
       :image_name, :slug, :tag_line, :price, :location, :activity, :orders
   has_many :hotels, :key => "hotel_ids", :root => "hotels"
   has_many :extras, :key => "extra_ids", :root => "extras"
