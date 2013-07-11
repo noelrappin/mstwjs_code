9887d95b18c42c17bbf87fb7154554885792acc3
Noel Rappin <noel@groupon.com>
2013-07-11 07:15:44 -0500

    oops

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -2,10 +2,7 @@ TimeTravel.Router.reopen({
   location: "history"
 });
 
-<<<<<<< HEAD
 //##jasmine
-=======
->>>>>>> e_04
 TimeTravel.Router.map(function() {
   this.route("index", {path: "/"});
   this.route("jasmine", {path: "/jasmine"});
