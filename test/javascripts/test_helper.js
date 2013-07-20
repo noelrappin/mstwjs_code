c49a6224618432451cedee95517ac8618f9d936b
Noel Rappin <noel@groupon.com>
2013-07-19 23:27:36 -0500

    formatting

--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -3,9 +3,13 @@
 //= require_tree .
 //= require_self
 //##start
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-document.write('<style>#ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
 
+document.write('<div id="ember-testing-container"><div id="ember-testing">' +
+    '</div></div>');
+document.write('<style>#ember-testing-container { position: absolute; ' +
+    'background: white; bottom: 0; right: 0; width: 640px; height: 384px; ' +
+    'overflow: auto; z-index: 9999; border: 1px solid #ccc; } ' +
+    '#ember-testing { zoom: 50%; }</style>');
 
 TimeTravel.rootElement = '#ember-testing';
 
