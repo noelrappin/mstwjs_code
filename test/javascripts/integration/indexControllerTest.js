549ad755c5b49c297450fb1e6785495ae068e9ea
Noel Rappin <noel@groupon.com>
2013-07-21 15:36:44 -0500

    spacing fix

--- test/javascripts/integration/indexControllerTest.js
+++ test/javascripts/integration/indexControllerTest.js
@@ -1,13 +1,7 @@
 module("Index Controller", {
   setup: function() {
-    TimeTravel.Trip.FIXTURES = [
-      {
-        id: 1,
-        name: "Mayflower",
-        startDate: "1620-09-06",
-        endDate: "1620-11-21"
-      }
-    ];
+    TimeTravel.Trip.FIXTURES = [ { id: 1, name: "Mayflower",
+        startDate: "1620-09-06", endDate: "1620-11-21" } ];
     Ember.run(TimeTravel, TimeTravel.advanceReadiness);
   },
   teardown: function() {
@@ -27,6 +21,6 @@ test("it manages clicks", function() {
   visit("/").then(function() {
     return click(".trip .header");
   }).then(function() {
-    equal($(".selected_name").text(), "Mayflower");
-  });
+        equal($(".selected_name").text(), "Mayflower");
+      });
 });
