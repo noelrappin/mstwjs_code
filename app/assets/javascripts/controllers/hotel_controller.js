3d5641ff1e7add5be85720e39a2604aaf761825c
Noel Rappin <noel@tablexi.com>
2013-09-07 23:19:09 -0500

    test cleanup

--- app/assets/javascripts/controllers/hotel_controller.js
+++ app/assets/javascripts/controllers/hotel_controller.js
@@ -5,18 +5,18 @@ TimeTravel.HotelController = Ember.ObjectController.extend({
     this._super();
   },
 
-  startEditing: function() {
-    this.set("nightsOrderedInput", String(this.get("nights_ordered")));
-    this.toggleProperty("editing");
-  },
+  actions: {
+    startEditing: function() {
+      this.set("nightsOrderedInput", String(this.get("nights_ordered")));
+      this.toggleProperty("editing");
+    },
 
-  //##transaction
-  endEditing: function() {
-    var transaction = this.get('store').transaction();
-    transaction.add(this.get('content'));
-    this.set("nights_ordered", parseInt(this.get("nightsOrderedInput")));
-    this.toggleProperty("editing");
-    transaction.commit();
+    //##transaction
+    endEditing: function() {
+      this.set("nights_ordered", parseInt(this.get("nightsOrderedInput")));
+      this.toggleProperty("editing");
+    }
+    //##transaction
   }
-  //##transaction
+
 })
--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -17,10 +17,10 @@ TimeTravel.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 //##fixture
 TimeTravel.Trip.FIXTURES = [
-  { id: 1, name: "Low", totalRevenue: 100,
-    startDate: "1620-09-06", endDate: "1620-11-21" },
-  { id: 2, name: "High", totalRevenue: 500,
-    startDate: "1600-09-06", endDate: "1600-09-07" }
+  { id: 1, name: "Low", orders: 1, price: 100,
+    start_date: "1620-09-06", end_date: "1620-11-21" },
+  { id: 2, name: "High", orders: 5, price: 100,
+    start_date: "1600-09-06", end_date: "1600-09-07" }
 ];
 //##fixture
 
