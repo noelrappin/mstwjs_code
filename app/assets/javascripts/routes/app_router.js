d4f083019c41c224f7034b033dd4bd4f80f55019
Noel Rappin <noel@tablexi.com>
2013-09-08 22:05:31 -0500

    remove jasmine route

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -6,5 +6,4 @@ TimeTravel.Router.map(function() {
   this.resource("trips", {path: "/trips"}, function() {
     this.resource("trip", {path: ":trip_id"});
   });
-  this.route("jasmine", {path: "/jasmine"});
 });
