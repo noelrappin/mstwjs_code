b2619109aff5f5c0efe2ee354d67359d6d5ead01
Noel Rappin <noel@tablexi.com>
2013-09-10 19:48:52 -0500

    getting silly

--- app/assets/javascripts/routes/trip_route.js
+++ app/assets/javascripts/routes/trip_route.js
@@ -2,4 +2,12 @@ TimeTravel.TripRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('trip', params.trip_id)
   }
+
+  actions: {
+    willTransition: function(transition) {
+      if(model.wontLetYouGo) {
+        transition.abort();
+      }
+    }
+  }
 });
